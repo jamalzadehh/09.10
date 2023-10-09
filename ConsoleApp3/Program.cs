dcd5ce671341b58d83061f6e86ae80a837315366
Your Name <you@example.com>
2023-10-09 04:43:29 +0400

    Fayl elave olundu

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,190 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            //Task2
+            //{
+            
+        //    Telebe reshaad = new Telebe("Reshaad", new int[] { 90, 85, 95 });
+        //    Telebe aydan = new Telebe("Aydan", new int[] { 92, 80, 90 });
+
+        //    Yarish competition = new Yarish(reshaad, aydan);
+
+        //    competition.CompareScores();
+
+        //}
+        //class Telebe
+        //{
+        //    public string Name { get; set; }
+        //    public int[] Scores { get; set; }
+
+        //    public Telebe(string name, int[] scores)
+        //    {
+        //        Name = name;
+        //        Scores = scores;
+        //    }
+        //}
+
+        //class Yarish
+        //{
+        //    public Telebe Reshaad { get; set; }
+        //    public Telebe Aydan { get; set; }
+
+        //    public Yarish(Telebe reshaad, Telebe aydan)
+        //    {
+        //        Reshaad = reshaad;
+        //        Aydan = aydan;
+        //    }
+
+        //    public void CompareScores()
+        //    {
+        //        int reshaadinxali = 0;
+        //        int aydaninxali = 0;
+
+        //        for (int i = 0; i < Reshaad.Scores.Length; i++)
+        //        {
+        //            if (Reshaad.Scores[i] > Aydan.Scores[i])
+        //            {
+        //                reshaadinxali++;
+        //            }
+        //            else if (Aydan.Scores[i] > Reshaad.Scores[i])
+        //            {
+        //                aydaninxali++;
+        //            }
+        //        }
+
+        //        Console.WriteLine(reshaadinxali);
+        //        Console.WriteLine(aydaninxali);
+        //    }
+        }
+
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+         //Task1
+         //    {
+        //        Student student = new Student("EliEhmed","Gulmemmedov","101A",83);
+        //        student.GetInfo();
+        //        student.CheckGraduation();
+        //        string DiplomDegree=student.GetDiplomDeggree();
+        //        Console.WriteLine(  DiplomDegree);
+
+
+
+        //    }
+        //}
+        //class Student
+        //{
+        //    public string Name;
+        //    public string Surname;
+        //    public string Group;
+        //    public int Point;
+        //    public bool IsGratuated;
+
+        //    public Student(string name, string surname,string group, int point ) 
+        //    {
+        //        Name = name; 
+        //        Surname = surname;  
+        //        Group = group;
+        //        Point = point;
+        //        IsGratuated = point > 60;
+
+        //    }
+        //    public void GetInfo()
+        //    {
+        //        Console.WriteLine(Name);
+        //        Console.WriteLine(Surname);
+        //        Console.WriteLine(Group);
+        //        Console.WriteLine(Point);
+        //    }
+        //    public void CheckGraduation()
+        //    {
+        //        if (IsGratuated) {
+        //            Console.WriteLine("Telebe mezun olub.");
+        //                }
+        //        else
+        //        {
+        //            Console.WriteLine("Telebe mezun ola bilmeyib.");
+        //        }
+
+        //    }
+        //    public string GetDiplomDeggree()
+        //    {
+        //        if (Point < 60)
+        //        {
+        //            return "Normadan ashagi bal";
+        //        }
+        //        else if (Point < 70)
+        //        {
+        //            return "Normal";
+        //        }
+        //        else if (Point < 80)
+        //        {
+        //            return "Honorable";
+        //        }
+        //        else
+        //        {
+        //            return "High Honorable";
+        //        }   
+        //    }
+
+
+    }
+    
+        
+
+   
+    
+	
+
+
+}
